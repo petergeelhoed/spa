8872de3deedeaaf8060339bf4ae9b2b13052f6d5
petergeelhoed <petergeelhoed@gmail.com>
2023-05-05 10:09:50 +0200

    spaces

--- spa.c
+++ spa.c
@@ -30,18 +30,18 @@ int main(int argc, char **argv)
         int secofday = epoch%86400;
         double julday = julian + 25569 + 2415018.5;
         double julcent = (julday - 2451545)/36525;
-        int modhelper = (int)(280.46646+julcent*(36000.76983 + julcent*0.0003032))/360.;
-        double geomMeanLong = (280.46646+julcent*(36000.76983 + julcent*0.0003032))-modhelper*360;
+        int modhelper = (int)(280.46646+julcent*(36000.76983+julcent*0.0003032))/360.;
+        double geomMeanLong = (280.46646+julcent*(36000.76983+julcent*0.0003032))-modhelper*360;
         double geomMeanAnom = 357.52911+julcent*(35999.05029-0.0001537*julcent);
         double eccentEarth = 0.016708634-julcent*(0.000042037+0.0000001267*julcent);
         double sunEqofCtr = sin(geomMeanAnom/RADPI)*(1.914602-julcent*(0.004817+0.000014*julcent))+sin((2*geomMeanAnom/RADPI))*(0.019993-0.000101*julcent)+sin((3*geomMeanAnom/RADPI))*0.000289;
-        double SunTrueLong =geomMeanLong + sunEqofCtr;
-        double SunTrueAnom =geomMeanAnom + sunEqofCtr;
+        double SunTrueLong =geomMeanLong+sunEqofCtr;
+        double SunTrueAnom =geomMeanAnom+sunEqofCtr;
         double sunRadVec = (1.000001018*(1-eccentEarth*eccentEarth))/(1+eccentEarth*cos(SunTrueAnom/RADPI));
         double sunAppLong = SunTrueLong-0.00569-0.00478*sin((125.04-1934.136*julcent)/RADPI);
         double meanOblElip = 23+(26+((21.448-julcent*(46.815+julcent*(0.00059-julcent*0.001813))))/60)/60;
         double OblCorr = meanOblElip+0.00256*cos((125.04-1934.136*julcent)/RADPI);
-        double sunRtAsc = atan2( cos((OblCorr/RADPI))*sin((sunAppLong/RADPI)) , cos((sunAppLong/RADPI)))*RADPI;
+        double sunRtAsc = atan2(cos((OblCorr/RADPI))*sin((sunAppLong/RADPI)),cos((sunAppLong/RADPI)))*RADPI;
         double sunDecl = (asin(sin((OblCorr/RADPI))*sin((sunAppLong/RADPI))))*RADPI;
         double vary = tan(OblCorr/RADPI/2)*tan(OblCorr/RADPI/2);
         double eqOfTime = (vary*sin(2*(geomMeanLong)/RADPI)-2*eccentEarth*sin((geomMeanAnom)/RADPI)+4*eccentEarth*vary*sin((geomMeanAnom)/RADPI)*cos(2*(geomMeanLong)/RADPI)-0.5*vary*vary*sin(4*(geomMeanLong)/RADPI)-1.25*eccentEarth*eccentEarth*sin(2*(geomMeanAnom)/RADPI))*4*RADPI;
@@ -49,10 +49,10 @@ int main(int argc, char **argv)
         double trueSolTime = ((double)secofday/60+eqOfTime+4*lng);
         modhelper = (int)(trueSolTime/1440);
         trueSolTime-=modhelper*1440;
-        double hourangle = (trueSolTime<0)?  trueSolTime/4+180: trueSolTime/4-180;
+        double hourangle = (trueSolTime<0)?trueSolTime/4+180:trueSolTime/4-180;
         double SolZenith = (acos(sin((lat)/RADPI)*sin((sunDecl)/RADPI)+cos((lat)/RADPI)*cos((sunDecl)/RADPI)*cos((hourangle)/RADPI)))*RADPI;
 
-        double solAzi = (hourangle > 0 )?
+        double solAzi = (hourangle >0)?
             (acos(((sin((lat)/RADPI)*cos((SolZenith)/RADPI))-sin((sunDecl)/RADPI))/(cos((lat)/RADPI)*sin((SolZenith)/RADPI))))*RADPI+180:
             180-(acos(((sin((lat)/RADPI)*cos((SolZenith)/RADPI))-sin((sunDecl)/RADPI))/(cos((lat)/RADPI)*sin((SolZenith)/RADPI))))*RADPI;
         printf("%ld %lf %lf\n",(secofday+(int)(timezone*3600))%86400,90-SolZenith,solAzi);
